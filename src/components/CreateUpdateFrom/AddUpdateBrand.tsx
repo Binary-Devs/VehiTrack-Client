ae46c7752a9da8eee5d91d58f23d53414d4db076
Khairul Alam <khairulislam38@gmail.com>
2024-01-09 02:12:23 +0600

    update modal, dashboard

--- src/components/CreateUpdateFrom/AddUpdateBrand.tsx
+++ src/components/CreateUpdateFrom/AddUpdateBrand.tsx
@@ -3,15 +3,11 @@ import Form from "@/components/Forms/Form";
 import FormInput from "@/components/Forms/FormInput";
 import {
   useCreateBrandMutation,
-  useGetSingleBrandQuery,
   useUpdateBrandMutation,
 } from "@/redux/api/brand/brandApi";
 import { Button, Col, Row, message } from "antd";
-import Loader from "../Utlis/Loader";
 
-const AddUpdateBrand = ({ id }: { id?: string }) => {
-  //Get
-  const { data, isLoading: getLoad } = useGetSingleBrandQuery(id ? id : "");
+const AddUpdateBrand = ({ updateData }: { updateData?: any }) => {
 
   //Update
   const [updateBrand, { isLoading: updateLoad }] = useUpdateBrandMutation();
@@ -19,16 +15,17 @@ const AddUpdateBrand = ({ id }: { id?: string }) => {
   //Create
   const [createBrand, { isLoading: createLoad }] = useCreateBrandMutation();
 
-  // console.log(id, data);
-
   const onSubmit = async (values: any) => {
-    message.loading(id ? "Updating...." : "Adding....");
+    message.loading(updateData ? "Updating...." : "Adding....");
     try {
-      const res = id
-        ? await updateBrand({ id, data: values }).unwrap()
+      const res = updateData
+        ? await updateBrand({ updateData, data: values }).unwrap()
         : await createBrand({ ...values }).unwrap();
+        console.log(res)
       if (res.id) {
-        message.success(`Brand ${id ? "updated" : "added"} successfully!`);
+        message.success(
+          `Brand ${updateData ? "updated" : "added"} successfully!`
+        );
       } else {
         message.error(res.message);
       }
@@ -37,17 +34,17 @@ const AddUpdateBrand = ({ id }: { id?: string }) => {
     }
   };
 
-  if (id && getLoad) {
-    return <Loader className="h-[40vh] flex items-center justify-center" />;
-  }
   return (
     <div>
       <h1 className="text-center my-1 font-bold text-2xl">
-        {id ? "Update Brand" : "Add Brand"}
+        {updateData ? "Update Brand" : "Add Brand"}
       </h1>
       {/*  */}
       <div>
-        <Form submitHandler={onSubmit} defaultValues={id ? { ...data } : {}}>
+        <Form
+          submitHandler={onSubmit}
+          defaultValues={updateData ? { ...updateData } : {}}
+        >
           <div
             style={{
               border: "1px solid #d9d9d9",
@@ -71,19 +68,19 @@ const AddUpdateBrand = ({ id }: { id?: string }) => {
                   name="label"
                   size="large"
                   label="Brand name"
-                  // defaultValue={data && data.label}
                   required={true}
                   placeholder="Please enter brand name"
                 />
               </Col>
             </Row>
-            <div className="flex justify-end items-center">
+            <div className="flex items-center">
               <Button
                 htmlType="submit"
                 type="primary"
                 disabled={createLoad || updateLoad}
+                style={{ width: "100%" }}
               >
-                {id ? "Update" : "Add"}
+                {updateData ? "Update" : "Add"}
               </Button>
             </div>
           </div>
